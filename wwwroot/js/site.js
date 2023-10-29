79c1bf542b1cb155f3c25726971bd777f69b6dbd
damiolabisi1 <89269563+damiolabisi1@users.noreply.github.com>
2023-10-29 03:10:11 +0000

    Working ftg + site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,7 +3,7 @@
 
 // Write your JavaScript code.
 
-let user = 0
+user = 0
 
 //id:{user_name,email,password,cart{id,item_name,price,item_amount,in_store}}
 //for cart [store_id,item_id,amount in cart,in_store]
@@ -63,248 +63,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// function loadproducts(store,product_div){
-//     let cat = store.catalog;
-//     let result = ""
-    
-
-//     // for each product
-//     Object.keys(cat).forEach(id =>{
-//         item = cat[id]
-//         console.log(item)
-
-//         //adds in the indevidual clothing items
-//         result += `<img src="path/to/image" alt="Black Shirt" width="50"></img> <div> <p>${item.item_name}</p> <p>Store: ${store.name}; M</p> <p>Size: Large</p> <p>Price: $${item.amount}</p> </div>`
-        
-//         // var x = document.getElementById("+");
-
-//     });
-//     // `<img src="path/to/image" alt="Black Shirt" width="50"></img>' '<div> <p>Black Shirt</p> <p>Store: H &amp; M</p> <p>Size: Large</p> <p>Price: $20</p> </div>`
-
-//     document.getElementById(product_div).innerHTML = result;
-
-
-// }
-
-// loadproducts(shops[0],"zara_products")
-// loadproducts(shops[1],"northface_products")
-// loadproducts(shops[2],"h&m_products")
-
-// function loadproducts(store,product_div){
-//     console.log(store);
-//     let st = shops[store]
-//     let cat = st.catalog;
-    
-//     let result = ""
-    
-
-//     // for each product
-//     Object.keys(cat).forEach(id =>{
-//         item = cat[id];
-//         // console.log(item);
-
-//         //adds in the indevidual clothing items
-//         result += `<img src="path/to/image" alt="Black Shirt" width="50"></img> <div> <p>${item.item_name}</p> <p>Store: ${store.name}; M</p> <p>Size: Large</p> <p>Price: $${item.amount}</p> </div>`;
-//         console.log("j1");
-//         result += `<div><button type="button" id="myBtn" onclick="${addtocart(store,id)}">+</button></div>`;
-//         console.log("j2");
-
-//     });
-//     // `<img src="path/to/image" alt="Black Shirt" width="50"></img>' '<div> <p>Black Shirt</p> <p>Store: H &amp; M</p> <p>Size: Large</p> <p>Price: $20</p> </div>`
-
-//     document.getElementById(product_div).innerHTML = result;
-
-
-// };
-
-// loadproducts(0,"zara_products");
-// loadproducts(1,"northface_products");
-// loadproducts(2,"h&m_products");
-
-// function addtocart(store_id,item_id){
-
-//     let incart = false;
-//     console.log("j3");
-
-//    for (const key in users[user]["cart"]){
-//         console.log("j4");
-//         if (users[user]["cart"][key][1]==item_id){
-//             console.log("j5");
-//             incart=true;
-
-//             users[user]["cart"][key][2]=users[user]["cart"][key][2]+1;
-//             //updatestorecart()
-
-//         };
-//     };
-
-//     if (incart==false){
-//         // addtostorecart();
-//         let keys = Object.keys(users[user]["cart"]);
-//         let newkey = keys;
-//         console.log("before add");
-//         console.log(users[user]["cart"]);
-//         users[user]["cart"][keys.length]=[store_id,item_id,1,false]
-//         console.log("after add");
-//         console.log(users[user]["cart"]);
-//     }  
-
-// };
-
-
-// function toggleActive(buttonId) {
-//     const buttons = document.querySelectorAll('.toggle-buttons button');
-//     buttons.forEach(button => {
-//         if (button.id === buttonId) {
-//             button.classList.add('active');
-//         } else {
-//             button.classList.remove('active');
-//         }
-//     });
-
-//     // Toggle content visibility based on the button pressed
-//     const deliveryContent = document.getElementById('main-content');
-//     const inStoreContent = document.getElementById('inStoreContent');
-//     if (buttonId === 'deliveryButton') {
-//         deliveryContent.style.display = 'flex';
-//         inStoreContent.style.display = 'none';
-//     } else if (buttonId === 'shoppingListButton') {
-//         deliveryContent.style.display = 'none';
-//         inStoreContent.style.display = 'block';
-//     }
-// }
-
-function toggleActive(buttonId) {
-    const inStoreContent = document.getElementById('inStoreContent');
-    const cartSection = document.getElementById('cart-section');
-
-    if (buttonId === 'shoppingListButton') {
-        inStoreContent.style.display = 'block';
-        cartSection.style.display = 'none';
-    } else {
-        inStoreContent.style.display = 'none';
-        cartSection.style.display = 'block';
-    }
-
-    const buttons = document.querySelectorAll('.toggle-buttons button');
-    buttons.forEach(button => {
-        if (button.id === buttonId) {
-            button.classList.add('active');
-        } else {
-            button.classList.remove('active');
-        }
-    });
-}
-
-// function filterStores() {
-//     // Get the input value and convert to lowercase for case-insensitive search
-//     const query = document.getElementById('storeSearchInput').value.toLowerCase();
-
-//     // Loop through each store in the shops data and filter based on the search query
-//     for (const shopId in shops) {
-//         const storeName = shops[shopId].name.toLowerCase();
-//         const storeElement = document.getElementById(`store-${shopId}`); 
-
-//         if (storeName.includes(query)) {
-//             storeElement.style.display = 'block'; // Show the store if it matches the query
-//         } else {
-//             storeElement.style.display = 'none'; // Hide the store if it doesn't match the query
-//         }
-//     }
-// }
-
-function filterStores() {
-    // 1. Get the search term
-    const searchTerm = document.getElementById('storeSearchInput').value.toLowerCase();
-
-    // 2. Loop through the list of stores
-    const stores = document.querySelectorAll('.store-name');
-    let storeFound = false; // Flag to check if any store matches the search term
-
-    stores.forEach(storeElement => {
-        const storeName = storeElement.textContent.toLowerCase();
-
-        if (storeName.includes(searchTerm)) {
-            storeElement.parentElement.style.display = 'block'; // Show the store if it matches
-            storeFound = true;
-        } else {
-            storeElement.parentElement.style.display = 'none'; // Hide the store if it doesn't match
-        }
-    });
-
-    // 3. Display a message if no stores match
-    const noResultsMessage = document.getElementById('noResultsMessage');
-    if (storeFound) {
-        noResultsMessage.style.display = 'none';
-    } else {
-        noResultsMessage.style.display = 'flex';
-    }
-}
-
-// Add event listener to the "Search" button
-// document.getElementById('searchButton').addEventListener('click', filterStores);
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     const cart = JSON.parse(localStorage.getItem('cart')) || [];
-//     const cartSection = document.getElementById('cart-section');
-
-//     // Clear cart section first
-//     while (cartSection.firstChild) {
-//         cartSection.removeChild(cartSection.firstChild);
-//     }
-
-//     // Populate the cart section with items from localStorage
-//     cart.forEach(productId => {
-//         // Here, you would fetch product details by productId (e.g., from an API or a data array)
-//         // and then create and append product elements to cartSection
-//         // For this example, let's just append a simple text node for each product
-
-//         const productElement = document.createElement('div');
-//         productElement.className = 'product';
-//         productElement.textContent = "Product ID: " + productId;
-//         cartSection.appendChild(productElement);
-//     });
-
-//     // Append the checkout section
-//     const checkoutDiv = document.createElement('div');
-//     checkoutDiv.className = 'checkout';
-//     checkoutDiv.innerHTML = '<button>Checkout</button><p>Total: $' + (cart.length * 20) + '</p>'; // Assuming each product costs $20 for simplicity
-//     cartSection.appendChild(checkoutDiv);
-// });
-
-
-// function loadproducts(store,product_div){
-//     // console.log(store);
-//     let st = shops[store]
-//     let cat = st.catalog;
-    
-//     let result = ""
-    
-
-//     // for each product
-//     Object.keys(cat).forEach(id =>{
-//         item = cat[id];
-//         // console.log(item);
-
-//         //adds in the indevidual clothing items
-//         result += `<img src="path/to/image" alt="Black Shirt" width="50"></img> <div> <p>${item.item_name}</p> <p>Store: ${st.name}; M</p> <p>Size: Large</p> <p>Price: $${item.amount}</p> </div>`;
-//         result += `<div><button type="button" id="myBtn" onclick="${addtocart(store,id)}">+</button></div>`;
-//         // result += `<div><button type="button" id="myBtn" class="add-to-cart"">+</button></div>`;
-        
-
-//     });
-//     // `<img src="path/to/image" alt="Black Shirt" width="50"></img>' '<div> <p>Black Shirt</p> <p>Store: H &amp; M</p> <p>Size: Large</p> <p>Price: $20</p> </div>`
-//     console.log(document.getElementById(product_div));
-//     document.getElementById(product_div).innerHTML = result;
-//     console.log(shops);
-//     console.log("Loading products for:", product_div);
-
-
-// };
-
-// loadproducts(0,"zara_products");
-// loadproducts(1,"northface_products");
-// loadproducts(2,"hm_products");
 
 function loadproducts(store, product_div) {
     // Check if the div exists in the DOM
@@ -342,79 +100,55 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-//////dami edited here
-// function addtocart(store_id,item_id){
+function toggleActive(buttonId) {
+    const inStoreContent = document.getElementById('inStoreContent');
+    const cartSection = document.getElementById('cart-section');
 
+    if (buttonId === 'shoppingListButton') {
+        inStoreContent.style.display = 'block';
+        cartSection.style.display = 'none';
+    } else {
+        inStoreContent.style.display = 'none';
+        cartSection.style.display = 'block';
+    }
 
-//     let incart = false;
+    const buttons = document.querySelectorAll('.toggle-buttons button');
+    buttons.forEach(button => {
+        if (button.id === buttonId) {
+            button.classList.add('active');
+        } else {
+            button.classList.remove('active');
+        }
+    });
+}
 
-//    for (const key in users[user]["cart"]){
-//         if (users[user]["cart"][key][1]==item_id){
-//             incart=true;
+function filterStores() {
+    // 1. Get the search term
+    const searchTerm = document.getElementById('storeSearchInput').value.toLowerCase();
 
-//             users[user]["cart"][key][2]=users[user]["cart"][key][2]+1;
-//             //updatestorecart()
+    // 2. Loop through the list of stores
+    const stores = document.querySelectorAll('.store-name');
+    let storeFound = false; // Flag to check if any store matches the search term
 
-//         };
-//     };
+    stores.forEach(storeElement => {
+        const storeName = storeElement.textContent.toLowerCase();
 
-//     if (incart==false){
-//         // addtostorecart();
-//         let keys = Object.keys(users[user]["cart"]);
-//         let newkey = keys;
-//         // console.log("before add");
-//         // console.log(users[user]["cart"]);
-//         users[user]["cart"][keys.length]=[store_id,item_id,1,false]
-//         // console.log("after add");
-//         // console.log(users[user]["cart"]);
-//     }  
+        if (storeName.includes(searchTerm)) {
+            storeElement.parentElement.style.display = 'block'; // Show the store if it matches
+            storeFound = true;
+        } else {
+            storeElement.parentElement.style.display = 'none'; // Hide the store if it doesn't match
+        }
+    });
 
-//     cartTotal();
-
-// };
-
-// function cartTotal(){//sums up the price of cloths stored in the cart
-//     console.log("runs");
-//     let ct = users[user]["cart"];//local cart variable
-//     let total = 0;//the total 
-//     // console.log(ct);
-
-//     // console.log(ct);
-
-//     for (key in ct){//loops over the cart
-//         // console.log(shops[ct[key][0]]["catalog"]);
-//         total+=shops[ct[key][0]]["catalog"][ct[key][1]]["amount"]*ct[key][2];
-//         // console.log("total: "+total);
-//     }
-//     // console.log(total);
-//     return total;
-// };
-
-// console.log(cartTotal());
-// // cartTotal()
-
-// function displayTotal(total_div){
-//     console.log("testing");
-//     let t = cartTotal();
-//     let result ="";
-//     result += `<p>Total: ${t}</p>`;
-//     document.getElementById(total_div).innerHTML = result;
-// }
-
-// displayTotal("zara_total");
-
-
-// function loadCartItems(cart_div) {
-//     // const cart = JSON.parse(localStorage.getItem('cart')) || [];
-//     document.getElementById(cart_div).innerHTML = "";
-//     let result = ""
-
-//     users[user]["cart"].forEach(product => {
-//         let item = shops[product[0]]["catalog"][product[1]];
-//         // result += ` <div> <p>${}</p> <p>Store: ${store.name}; M</p> <p>Size: Large</p> <p>Price: $${item.amount}</p> </div>`;
-        
-//     });
-// };
+    // 3. Display a message if no stores match
+    const noResultsMessage = document.getElementById('noResultsMessage');
+    if (storeFound) {
+        noResultsMessage.style.display = 'none';
+    } else {
+        noResultsMessage.style.display = 'flex';
+    }
+}
 
 function addtocart(store_id,item_id){
 
@@ -535,5 +269,3 @@ function spec_cart(s,cart_div){
 
 displayTotal("cart_total");
 loadCartItems("total_cart")
-
-
